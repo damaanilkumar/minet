e27871d8c4b0a67fd4d3ff80621ad37041e8ac4b
damaanilkumar <anilkumar.dama@zemosolabs.com>
2022-05-28 14:20:40 +0530

    Anil

--- src/Components/Molecules/Charts/Charts.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Charts from "./Charts";
-
-test("Check the values in the card", () => {
- render( < Charts selectedCoinName='BITCOIN'/>)
-        const typoElement = screen.getByText("My portfolio value");
-        expect(typoElement).toBeInTheDocument();
-      }); 
